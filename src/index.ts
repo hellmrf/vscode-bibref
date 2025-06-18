6df765081913de47092a930b32f7b626111ca01d
Heliton Martins <helitonmrf@gmail.com>
2025-06-17 23:03:27 -0300

    Register first command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { defineExtension } from 'reactive-vscode'
-import { window } from 'vscode'
+import { defineExtension, useCommand } from 'reactive-vscode'
+import * as vscode from 'vscode'
 
 const { activate, deactivate } = defineExtension(() => {
-  window.showInformationMessage('Hello')
+  vscode.window.showInformationMessage('Hello')
+
+  useCommand('bibref.hello', () => {
+    vscode.window.showInformationMessage('Hello World!')
+  })
 })
 
 export { activate, deactivate }
