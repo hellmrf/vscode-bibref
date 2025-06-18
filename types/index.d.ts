4d8ff78ba22486cf45fc42b43b8dcb25f081c54b
Heliton Martins <helitonmrf@gmail.com>
2025-06-18 04:11:58 -0300

    Fix linter warning

--- types/index.d.ts
+++ types/index.d.ts
@@ -26,39 +26,39 @@ declare module '@citation-js/core' {
     }
   }
 
-  type CSLItemType =
-    | 'article' // Generic article
-    | 'article-journal' // Journal article
-    | 'article-magazine' // Magazine article
-    | 'article-newspaper' // Newspaper article
-    | 'book' // Book
-    | 'chapter' // Book chapter
-    | 'entry' // Dictionary or encyclopedia entry
-    | 'entry-dictionary' // Dictionary entry
-    | 'entry-encyclopedia' // Encyclopedia entry
-    | 'manuscript' // Manuscript
-    | 'paper-conference' // Conference paper
-    | 'patent' // Patent
-    | 'report' // Report
-    | 'thesis' // Thesis
-    | 'webpage' // Webpage
-    | 'bill' // Bill
-    | 'case' // Legal case
-    | 'graphic' // Graphic
-    | 'interview' // Interview
-    | 'legislation' // Legislation
-    | 'motion_picture' // Motion picture (film)
-    | 'song' // Song
-    | 'speech' // Speech
-    | 'map' // Map
-    | 'broadcast' // Broadcast (e.g., TV show, podcast)
-    | 'personal_communication' // Personal communication
-    | 'software' // Software
-    | 'dataset' // Dataset
-    | 'figure' // Figure
-    | 'post' // Post (e.g., blog post)
-    | 'document' // Generic document
-    | 'other' // Other types not covered above
+  type CSLItemType
+    = | 'article' // Generic article
+      | 'article-journal' // Journal article
+      | 'article-magazine' // Magazine article
+      | 'article-newspaper' // Newspaper article
+      | 'book' // Book
+      | 'chapter' // Book chapter
+      | 'entry' // Dictionary or encyclopedia entry
+      | 'entry-dictionary' // Dictionary entry
+      | 'entry-encyclopedia' // Encyclopedia entry
+      | 'manuscript' // Manuscript
+      | 'paper-conference' // Conference paper
+      | 'patent' // Patent
+      | 'report' // Report
+      | 'thesis' // Thesis
+      | 'webpage' // Webpage
+      | 'bill' // Bill
+      | 'case' // Legal case
+      | 'graphic' // Graphic
+      | 'interview' // Interview
+      | 'legislation' // Legislation
+      | 'motion_picture' // Motion picture (film)
+      | 'song' // Song
+      | 'speech' // Speech
+      | 'map' // Map
+      | 'broadcast' // Broadcast (e.g., TV show, podcast)
+      | 'personal_communication' // Personal communication
+      | 'software' // Software
+      | 'dataset' // Dataset
+      | 'figure' // Figure
+      | 'post' // Post (e.g., blog post)
+      | 'document' // Generic document
+      | 'other' // Other types not covered above
 
   interface CSLJSON {
     abstract?: string
