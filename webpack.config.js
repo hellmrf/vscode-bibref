ba35454f1b6d48b22e5a18b8bc39816baa6f8ff2
Heliton Martins <helitonmrf@gmail.com>
2025-06-18 21:58:04 -0300

    Fix dependencies bundling

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('node:path')
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: {
+    vscode: 'commonjs vscode',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: 'nosources-source-map',
+}
